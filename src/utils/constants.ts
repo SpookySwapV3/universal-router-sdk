6948f166ac39acf334b0908d628d854311d055fe
allvme <90796822+allvme@users.noreply.github.com>
2025-05-16 02:39:52 +0200

    Add new chains

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -53,9 +53,9 @@ const CHAIN_CONFIGS: { [key: number]: ChainConfig } = {
   },
   // arbitrum
   [42161]: {
-    router: '0xeC8B0F7Ffe3ae75d7FfAb09429e3675bb63503e4',
+    router: '0x597ef0f1628DCCe33119588053aF91d68B8D2069',
     weth: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
-    creationBlock: 125861718,
+    creationBlock: 334651111,
   },
   // arbitrum goerli
   [421613]: {
@@ -83,9 +83,9 @@ const CHAIN_CONFIGS: { [key: number]: ChainConfig } = {
   },
   // avalanche
   [43114]: {
-    router: '0x82635AF6146972cD6601161c4472ffe97237D292',
+    router: '0xD80E8cc5e1Ea13bE9930407c1441da0Fd9F6555e',
     weth: '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7',
-    creationBlock: 34491144,
+    creationBlock: 61944729,
   },
   // base goerli
   [84531]: {
@@ -95,9 +95,9 @@ const CHAIN_CONFIGS: { [key: number]: ChainConfig } = {
   },
   // base mainnet
   [8453]: {
-    router: '0xeC8B0F7Ffe3ae75d7FfAb09429e3675bb63503e4',
+    router: '0x1774b8Dc8648189c0884FD1fa79aa054Ced9eFb5',
     weth: '0x4200000000000000000000000000000000000006',
-    creationBlock: 3229053,
+    creationBlock: 30005742,
   },
   // bittorrent mainnet
   [199]: {
@@ -149,6 +149,19 @@ const CHAIN_CONFIGS: { [key: number]: ChainConfig } = {
     weth: '0x760AfE86e5de5fa0Ee542fc7B7B713e1c5425701',
     creationBlock: 7769129,
   },
+  // bera
+  [80094]: {
+    router: '0xD80E8cc5e1Ea13bE9930407c1441da0Fd9F6555e',
+    weth: '0x6969696969696969696969696969696969696969',
+    creationBlock: 4952267,
+  },
+  // mantle
+  [5000]: {
+    router: '0x1774b8Dc8648189c0884FD1fa79aa054Ced9eFb5',
+    weth: '0x78c1b0C915c4FAA5FffA6CAbf0219DA63d7f4cb8',
+    creationBlock: 79501317,
+  },
+
 }
 
 export const UNIVERSAL_ROUTER_ADDRESS = (chainId: number): string => {
